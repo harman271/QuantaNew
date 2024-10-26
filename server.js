d314d861f02033f12d782c9ee648eb6e9dc7555e
harman271 <harma32323@gmail.com>
2024-10-27 00:37:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const fetch = require('node-fetch');
 const path = require('path');
 
 const app = express();
-const PORT =3000;
+const PORT = process.env.PORT || 3000;  // Use process.env.PORT for Vercel
 
 // Store multiple API keys if needed
-const apiKeys = ['2dd75c8da8ca46368966099abbe852a2, 65b479aea55047529eef5f407c431d60','8a980c0539164f38af6463cdbe651cf1','543b3f2fe08449b183c4abe5150f2f25'];
+const apiKeys = [
+    '2dd75c8da8ca46368966099abbe852a2',
+    '65b479aea55047529eef5f407c431d60',
+    '8a980c0539164f38af6463cdbe651cf1',
+    '543b3f2fe08449b183c4abe5150f2f25'
+];
 let currentApiKeyIndex = 0;
 
 // Serve static files like CSS, JavaScript, and HTML
@@ -67,27 +72,3 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
